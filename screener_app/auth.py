9e7678c4a4f561106cd79e4e3831ae728d33c77d
Jon Bos <jbos@mumosystems.com>
2021-08-01 09:57:25 -0500

    misc: formatting

--- screener_app/auth.py
+++ screener_app/auth.py
@@ -7,7 +7,6 @@ from screener_app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix="/auth")
 
-
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
