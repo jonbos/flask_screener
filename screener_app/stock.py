8f0d0d985e6ce6cdf14d8c7b3cbef99ef07952b3
Jon Bos <jbos@mumosystems.com>
2021-07-31 20:15:02 -0500

    convert server-side stock index page to api endpoint

--- screener_app/stock.py
+++ screener_app/stock.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
+import json
 
-bp = Blueprint('stocks', __name__, url_prefix="/stocks")
+import plotly as plotly
+import plotly.express as px
+import yfinance as yf
+from flask import Blueprint, render_template, request
+
+bp = Blueprint('stocks', __name__, url_prefix="")
 
 
-@bp.route("", methods=("GET",))
-def stock_index():
-    return render_template("stocks/index.html")
+@bp.route("/api/stock", methods=("GET",))
+def stock_api():
+    ticker = request.args.get('ticker', default='SPY')
+    period = request.args.get('period', default='1Y')
+    interval = request.args.get('interval', default='1D')
+    return gm(stock=ticker, period=period, interval=interval)
+
+
+def gm(stock, period, interval):
+    st = yf.Ticker(stock)
+
+    # Create a line graph
+    df = st.history(period=(period), interval=interval)
+    df = df.reset_index()
+    df.columns = ['Date-Time'] + list(df.columns[1:])
+    max = (df['Open'].max())
+    min = (df['Open'].min())
+    range = max - min
+    margin = range * 0.05
+    max = max + margin
+    min = min - margin
+    fig = px.area(df, x='Date-Time', y="Open",
+                  hover_data=("Open", "Close", "Volume"),
+                  range_y=(min, max), template="seaborn")
+
+    # Create a JSON representation of the graph
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
